da1b8a042308e2ae4e080fb42e788e8e1d6b414d
Daniel de castro <102565588+estudentDaniel@users.noreply.github.com>
2022-08-23 21:04:07 -0300

    inicio de aula

---
+++ hello_word/hello_word/lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const hello_word());
+}
+
+class hello_word extends StatelessWidget {
+  const hello_word({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
