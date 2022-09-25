aa06a3da32634daee2ce0bd687f87cb4cc900e6b
Daniel de castro <102565588+estudentDaniel@users.noreply.github.com>
2022-09-25 13:30:28 -0300

    final task

--- lista_tarefas/lib/home_screnn.dart
+++ lista_tarefas/lib/home_screnn.dart
@@ -2,15 +2,17 @@ import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:lista_tarefas/cadastro.dart';
+import 'package:lista_tarefas/tarefa.dart';
 
 class Home_screen extends StatefulWidget {
-  const Home_screen({super.key});
+  Home_screen({super.key});
 
   @override
   State<Home_screen> createState() => _Home_screenState();
 }
 
 class _Home_screenState extends State<Home_screen> {
+  List<Task> _list = List.empty(growable: true);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,11 +20,35 @@ class _Home_screenState extends State<Home_screen> {
         title: Text("Home Screen"),
         backgroundColor: Theme.of(context).primaryColor,
       ),
-      body: Container(),
+      body: ListView.separated(
+          itemBuilder: (context, position) {
+            Task item = _list[position];
+            return ListTile(
+              title: Text(
+                _list[position].text,
+                style: TextStyle(
+                  color: item.done ? Colors.green : Colors.black,
+                  decoration: item.done
+                      ? TextDecoration.lineThrough
+                      : TextDecoration.none,
+                ),
+              ),
+              onTap: () {
+                setState(() {
+                  item.done = !item.done;
+                });
+              },
+            );
+          },
+          separatorBuilder: (context, index) => Divider(),
+          itemCount: _list.length),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          Navigator.push(
+        onPressed: () async {
+          Task todo = await Navigator.push(
               context, MaterialPageRoute(builder: (context) => Cadastro()));
+          setState(() {
+            _list.add(todo);
+          });
         },
         child: Icon(Icons.add),
       ),
