51d096a4fcf9f7d607e3d01508177f18605f0ae3
Daniel de castro <102565588+estudentDaniel@users.noreply.github.com>
2022-11-02 14:16:36 -0300

    appro

---
+++ Promocões/appromocoes/android/app/src/main/kotlin/com/example/appromocoes/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.appromocoes
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- vendas_plus/android/app/src/main/kotlin/com/example/vendas_plus/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.vendas_plus
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
