8a51e001e99ee25422d85de1c5be436a8ed13c09
Daniel de castro <102565588+estudentDaniel@users.noreply.github.com>
2022-12-15 22:07:45 -0300

    .

---
+++ appromocao/appromocao/lib/anuncios.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class Anuncio extends StatefulWidget {
+  const Anuncio({super.key});
+
+  @override
+  State<Anuncio> createState() => _AnuncioState();
+}
+
+class _AnuncioState extends State<Anuncio> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Anuncio"),
+      ),
+      body: ListView(),
+    );
+  }
+}
