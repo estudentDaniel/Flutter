49551803bc0d62f45d36f5ca1d75cce764fdfe1a
Daniel de castro <102565588+estudentDaniel@users.noreply.github.com>
2022-12-15 21:57:03 -0300

    inico

--- appromocao/appromocao/lib/Login.dart
+++ appromocao/appromocao/lib/Login.dart
@@ -1,31 +1,67 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
 
-class FieldTitle extends StatelessWidget {
-  FieldTitle({required this.title, required this.subtitle});
-
-  final String title;
-  final String subtitle;
-
+class Login extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.only(left: 3, bottom: 4),
-      child: Wrap(
-        crossAxisAlignment: WrapCrossAlignment.end,
-        children: [
-          Text(
-            "$title: ",
-            style: const TextStyle(color: Colors.blue, fontSize: 20),
-          ),
-          Text(
-            "$subtitle: ",
-            style: const TextStyle(color: Color.fromARGB(255, 19, 19, 20)),
-          ),
-        ],
-      ),
-    );
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("login"),
+          centerTitle: true,
+        ),
+        body: Container(
+            alignment: Alignment.center,
+            child: SingleChildScrollView(
+              child: Padding(
+                  padding: const EdgeInsets.only(bottom: 16),
+                  child: Card(
+                      margin: const EdgeInsets.symmetric(horizontal: 32),
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(16)),
+                      elevation: 8,
+                      child: Padding(
+                        padding: const EdgeInsets.all(16),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.stretch,
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            const TextField(
+                              decoration: InputDecoration(
+                                border: OutlineInputBorder(),
+                                hintText: "USUARIO:",
+                                isDense: true,
+                              ),
+                              keyboardType: TextInputType.emailAddress,
+                            ),
+                            SizedBox(
+                              height: 16,
+                            ),
+                            TextField(
+                              decoration: InputDecoration(
+                                border: OutlineInputBorder(),
+                                hintText: 'SENHA:',
+                                isDense: true,
+                              ),
+                              keyboardType: TextInputType.text,
+                              autocorrect: false,
+                              obscureText: true,
+                            ),
+                            const SizedBox(
+                              height: 16,
+                            ),
+                            Divider(
+                              color: Color.fromARGB(255, 143, 143, 143),
+                            ),
+                            ElevatedButton(
+                                onPressed: () {
+                                  Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                          builder: (context) => Login()));
+                                },
+                                child: Text("Entrar"))
+                          ],
+                        ),
+                      ))),
+            )));
   }
 }
--- appromocao/appromocao/lib/main.dart
+++ appromocao/appromocao/lib/main.dart
@@ -1,30 +1,9 @@
-import 'package:flutter/foundation.dart';
+import 'package:appromocao/cadastro.dart';
+import 'package:appromocao/login.dart';
 import 'package:flutter/material.dart';
-import 'package:get_it/get_it.dart';
-import 'package:appromocao/page_store.dart';
-import 'package:parse_server_sdk_flutter/parse_server_sdk.dart';
-
-import 'home_screen.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await initializeParser();
-  setupLocations();
   runApp(MyApp());
-}
-
-void setupLocations() {
-  GetIt.I.registerSingleton(PageStore());
-}
-
-Future<void> initializeParser() async {
-  await Parse().initialize(
-    'Q4GiMgtiVwrKjMUhkMXy4HGSvxJY6THynn0ulN4i',
-    'https://parseapi.back4app.com/',
-    clientKey: 'Mg6Ts8y8rGp0DxZL2UHXk8cCNP2Po63x50I87UkQ',
-    autoSendSessionId: true,
-    debug: true,
-  );
 }
 
 class MyApp extends StatelessWidget {
@@ -40,7 +19,7 @@ class MyApp extends StatelessWidget {
         visualDensity: VisualDensity.adaptivePlatformDensity,
         scaffoldBackgroundColor: Color.fromARGB(255, 234, 242, 248),
       ),
-      home: HomeScreen(),
+      home: Cadastro(),
     );
   }
 }
