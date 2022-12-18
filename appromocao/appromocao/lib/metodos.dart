7b79d16a759c6e224ee2772fe7d5fd3395cfec07
Daniel de castro <102565588+estudentDaniel@users.noreply.github.com>
2022-12-18 16:58:12 -0300

    arrumado

--- appromocao/appromocao/lib/metodos.dart
+++ appromocao/appromocao/lib/metodos.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'UrlAPi.dart' as apiUrl;
 import 'package:http/http.dart' as http;
@@ -28,23 +29,24 @@ class Methods {
   }
 
   //------------------------------------------------------------------------
-  Future<bool> adicionar() async {
+  Future<task> adicionar(task addAnun) async {
     SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
     var token = sharedPreferences.getString('token');
     var urlApi = apiUrl.URl;
 
     var api = Uri.parse('${urlApi}/anuncios/');
-    var response = await http.post(api, headers: {
-      'Authorization': '$token',
-    });
+    var response = await http.post(api,
+        headers: {
+          'Authorization': 'Bearer $token',
+        },
+        body: toJson(addAnun));
 
     if (response.statusCode == 201) {
-      print(jsonDecode(response.body));
-
-      return true;
+      task addAnun = addfromJson(response.body);
+      return addAnun;
     } else {
-      print(jsonDecode(response.body));
-      return false;
+      Exception("quebro");
     }
+    return addAnun;
   }
 }
--- appromocao/appromocao/lib/task.dart
+++ appromocao/appromocao/lib/task.dart
@@ -1,8 +1,13 @@
+import 'dart:convert';
+
+task addfromJson(String str) => task.fromJson(json.decode(str));
+String toJson(task data) => json.encode(data.toMap());
+
 class task {
   String id;
-  String? titulo;
-  String? descricao;
-  double? preco;
+  String titulo;
+  String descricao;
+  double preco;
 
   task(
       {required this.id,
@@ -10,17 +15,12 @@ class task {
       required this.descricao,
       required this.preco}); //constructor
 
-  factory task.fromJson(Map<String, dynamic> map) => task(
-        id: map['id'],
-        titulo: map['titulo'],
-        descricao: map['descricao'],
-        preco: map['preco'],
+  factory task.fromJson(Map<String, dynamic> json) => task(
+        id: json['id'],
+        titulo: json['titulo'],
+        descricao: json['descricao'],
+        preco: json['preco'],
       );
   Map<String, dynamic> toMap() =>
       {"id": id, "titulo": titulo, "descricao": descricao, "preco": preco};
-
-  // @override
-  // String toString() {
-  //   return "task(id: $id, titulo: $titulo, descricao: $descricao, preco: $preco";
-  // }
 }
